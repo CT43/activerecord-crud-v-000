cafc96f049a6b4e95bc9e06b0ad638dafcc5a2eb
Christian Thomas <christian.thomas.2894@gmail.com>
2018-07-24 22:43:24 +0000

    automatically backed up by learn

--- db/migrate/20180724223948_create_movies.rb
+++ db/migrate/20180724223948_create_movies.rb
@@ -1,4 +1,10 @@
 class CreateMovies < ActiveRecord::Migration
   def change
+    create_table :movies do |t| 
+      t.string :title 
+      t.datetime :release_date 
+      t.sting :director 
+      t.string :lead 
+      in_theatres :boolean
   end
 end
